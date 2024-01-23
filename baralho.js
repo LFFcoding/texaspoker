159491bfef8446a5a8df3a5541dd0e794483d601
LFFcoding <leofreitapro@yahoo.com.br>
2024-01-23 20:44:10 -0300

    initial tests

---
+++ baralho.js
@@ -0,0 +1,12 @@
+let baralho = {};
+for(let i = 0; i < 52; i++){
+    baralho[i] = {
+        noBaralho: true,
+        userId: null,
+        naMesa: false,
+    };
+};
+
+
+
+console.log(baralho);
